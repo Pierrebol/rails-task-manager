584f72b39b1460badd92ec0f5c031a45e9251bb8
pierrebollack <pierre.bollack@edhec.com>
2021-06-08 11:50:37 +0200

    add db task

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_08_093540) do
+ActiveRecord::Schema.define(version: 2021_06_08_094328) do
+
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.boolean "completed"
+    t.boolean "completed", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "important", default: false
   end
+
 end
