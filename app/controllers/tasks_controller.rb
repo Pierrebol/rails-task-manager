e878e2c8beeaae0153ad089e19bea1608e0f2076
pierrebollack <pierre.bollack@edhec.com>
2021-06-08 13:25:35 +0200

    create a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,4 +11,20 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+
+  def create
+    task = Task.new(task_params)
+
+    if task.save
+      redirect_to task_path(task)
+    else
+      raise
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :important)
+  end
 end
